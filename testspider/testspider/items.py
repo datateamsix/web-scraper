09a65f516749e843f3fc6993fff36869da791bfe
Madpy_ <md@datateamsix.com>
2024-09-15 01:18:44 +0000

    items and data pipeline handling

--- testspider/testspider/items.py
+++ testspider/testspider/items.py
@@ -1,5 +1,3 @@
-# Define here the models for your scraped items
-#
 # See documentation in:
 # https://docs.scrapy.org/en/latest/topics/items.html
 
@@ -8,5 +6,24 @@ import scrapy
 
 class TestspiderItem(scrapy.Item):
     # define the fields for your item here like:
-    # name = scrapy.Field()
+    name = scrapy.Field()
     pass
+
+#Data Serializers for the BookItem, utilize to format and normalize the data
+#def price_serializer(value):
+    #return value.split('£')[1]
+
+class BookItem(scrapy.Item):
+   url = scrapy.Field()
+   title = scrapy.Field()
+   upc = scrapy.Field()
+   product_type = scrapy.Field()
+   price_excl_tax = scrapy.Field()
+   price_incl_tax = scrapy.Field()
+   tax = scrapy.Field()
+   availability = scrapy.Field()
+   num_reviews = scrapy.Field()
+   stars = scrapy.Field()
+   category = scrapy.Field()
+   description = scrapy.Field()
+   price = scrapy.Field()
